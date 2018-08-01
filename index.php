e52b7d66e8d4e5c7018d658b6cbd2f1c548e5e42
Ocielorte <altek23@hotmail.com>
2018-07-31 21:19:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="es">
+
+	<header>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script src="http://code.jquery.com./jquery-lastest.min.js"></script>
+		<link href="css/fronted.css" rel="stylesheet" type="text/css" />
+		<script src="http://code.jquery.com./jquery-lastest.min.js"></script>
+	<title>All For Digital World</title>
+	</header>
+
+<body>
+	<div class="container">
+		<div class="header">
+			<?php include("php/encabezado.php"); ?>
+	<p><p>
+		<center>
+	<a href="productos.php">
+	<img src="Galerias/productos.png" width="150" height="40">
+	</a>
+	<a href="servicios.php">
+	<img src="Galerias/servicios.png" width="150" height="40">
+	</a>
+	<a href="mision.php">
+	<img src="Galerias/mision.png" width="150" height="40">
+	</a>
+	<a href="http://www.internetglosario.com/">
+	<img src="Galerias/glosario.png" width="150" height="40">
+	</a>
+	<a href="contacto.php">
+	<img src="Galerias/contacto.png" width="150" height="40">
+	</a>
+</center>
+</div><!-- fin encabezado -->
+	<center>
+		<h1><font face="Arial">Bienvenidos</font></h1></center>
+	<center>
+<video src="ejemplo.mp4" width="620" height="440" controls poster="vistaprevia.jpg">
+	lo sentimos no puedes reproducir <br>
+la version descargable esta en : <a href=" https://www.youtube.com/watch?v=hGynMrA-H80">enlace</a>
+</video>
+</center>
+	<center>
+		<h1><font face="Arial">Los Expertos en Soporte Tecnico Y Mantenimiento</font></h1>
+	</center>
+	<div class="footer">
+		<center>
+			<td><font color="ffffff" face="Arial">INICIO</font></td>
+		</center>
+	<center>
+		<table>
+		<tr>
+			<td><font color="000000" face="Arial">correo: altek23@hotmail.com</font></td>
+		</tr>
+	</table>
+	</center>
+	</div><!-- fin footer -->
+</div><!-- fin body -->
+</div>
+</div>
+</body>
+</html>
---
+++ php/encabezado.php
@@ -0,0 +1,1 @@
+<img src="Galerias/mi logo1.1.png" width="550" height="200" />
